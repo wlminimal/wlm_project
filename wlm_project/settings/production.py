fbc243602e6079ec1f23f4beac03a69f3da608dc
taedori <wlminimal@gmail.com>
2017-04-13 20:51:19 -0700

    requirements.txt fix

--- wlm_project/settings/production.py
+++ wlm_project/settings/production.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import, unicode_literals
+from boto.s3.connection import OrdinaryCallingFormat
+from django.utils import six
 
+import logging
 from .base import *
 
 DEBUG = False
