a396962aeaf1e1f1eeddf7bbb84eca22ffc23bab
taedori <wlminimal@gmail.com>
2017-04-14 08:13:48 -0700

    Amazon s3 setup

--- wlm_project/settings/base.py
+++ wlm_project/settings/base.py
@@ -148,8 +148,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 
 # Wagtail settings
--- wlm_project/settings/dev.py
+++ wlm_project/settings/dev.py
@@ -10,6 +10,8 @@ SECRET_KEY = 'ymj=@err5qf5y4vyru1c*r4!vk=!5ot(v+6x@u1vii$w5e0#ei'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 try:
