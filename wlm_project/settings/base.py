411caae2854d150998b5c422ea013df2b90ae5c6
taedori <wlminimal@gmail.com>
2017-04-13 21:28:09 -0700

    production.py fix

--- wlm_project/settings/base.py
+++ wlm_project/settings/base.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
